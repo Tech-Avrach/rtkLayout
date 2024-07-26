f56243a09d648ffa49a335275024b7df80506dea
au8085 <upadhyay.aditya8085@gmail.com>
2024-07-26 21:56:38 +0530

    validation code updated

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -4,30 +4,34 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 // import { Checkbox, Input, Label, Button } from "@/components/ui";
-import {  Input, Button } from "@/components/ui";
+import { Input, Button } from "@/components/ui";
 
 // Define your form schema using Zod
-
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
 
 const formSchema = z.object({
-  email: z.string().email("Invalid email address").refine((value) => value !== "", {
-    message: "Email is required",
-    path: ["email"],
-  }),
-  password: z.string().refine((value) => value !== "", {
-    message: "password is required",
-    path: ["password"],
-  }),
+  email: z
+    .string()
+    .regex(emailRegex, { message: "Invalid email address" })
+    .min(1, { message: "Email is required" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .regex(passwordRegex, {
+      message:
+        "Password must include at least one uppercase letter, one lowercase letter, one number, and one special character",
+    }),
 });
-
 const Login = () => {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -36,58 +40,61 @@ const Login = () => {
       email: "",
       password: "",
     },
-  })
+  });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-login-gradient">
       <div className="bg-white p-8 rounded-lg shadow-2xl max-w-lg w-full">
-        <h2 className="mb-6 text-center text-xl">Please sign in to your account below.</h2>
+        <h2 className="mb-6 text-center text-xl">
+          Please sign in to your account below.
+        </h2>
         <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} >
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>email</FormLabel>
-              <FormControl>
-                <Input placeholder="email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>password</FormLabel>
-              <FormControl>
-                <Input placeholder="password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex items-center justify-between mt-4">
-            <Link to={""} className="text-primary hover:underline text-sm">Recover Password</Link>
-            <Button type="submit" variant="default">Login to Dashboard</Button>
-          </div>
-      </form>
-    </Form>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-between mt-4">
+              <Link to={""} className="text-primary hover:underline text-sm">
+                Recover Password
+              </Link>
+              <Button type="submit" variant="default">
+                Login to Dashboard
+              </Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </div>
-    
   );
 };
-
-
 
 export default Login;
